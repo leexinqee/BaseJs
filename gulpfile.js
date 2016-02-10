ed385c25de4784db71d3e474c1b013dfd234c162
leexinqee <leexinqee@163.com>
2016-02-11 03:28:45 +0800

    update basejs

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,23 @@
 // 引入模块
 var gulp = require('gulp'),
-    uglify = require('gulp-uglify');
+    uglify = require('gulp-uglify'),
+    concat = require('gulp-concat');
  
-//定义一个默认任务
-gulp.task('default', function(){
-    gulp.src(['src/js/*.js'])
+//定义一个concat任务
+gulp.task('concat', function(){
+    gulp.src(['src/js/tool.js', 'src/js/base.js'])
+    	.pipe(concat("Base.js"))
         .pipe(uglify())
         .pipe(gulp.dest('dist/js'));
 });
+
+//定义一个plugin任务
+gulp.task('plugin', function(){
+    gulp.src(['src/js/base_drag.js'])
+        .pipe(uglify())
+        .pipe(gulp.dest('dist/js'));
+});
+
+//定义一个默认任务
+gulp.task('default', ['concat', 'plugin']);
+
